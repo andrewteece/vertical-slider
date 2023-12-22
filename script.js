eaef3457bf8ecefc1d51ae14886c3b8472aac28f
Andrew Teece <andrewteece@gmail.com>
2023-12-22 17:45:40 -0600

    js script complete and tested

---
+++ script.js
@@ -0,0 +1,32 @@
+const sliderWrapper = document.querySelector('.wrapper')
+const slideRight = document.querySelector('.right--slide')
+const slideLeft = document.querySelector('.left--slide')
+const upButton = document.querySelector('.slider__button--up')
+const downButton = document.querySelector('.slider__button--down')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderWrapper.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
